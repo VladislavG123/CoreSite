31422b02670e2d3ae8e06ba15d95b685dc57ee23
Владислав Горохов <gbg.0101@mail.ru>
2019-11-14 18:01:00 +0600

    first commit

---
+++ CoreSite/DataAccess/ShopContext.cs
@@ -0,0 +1,26 @@
+﻿using CoreSite.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreSite.DataAccess
+{
+    public class ShopContext :DbContext
+    {
+        public ShopContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Product> Payments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Cost = 2300, Name = "Наушники", Descripion = "Супер пупер наушники. Топ звук" },
+                new Product { Cost = 200, Name = "Овсянка", Descripion = "Лучшая овсянка" },
+                new Product { Cost = 300000, Name = "IPhone12", Descripion = "Почти оригинал" });
+        }
+    }
+}
---
+++ CoreSite/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreSite.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public double Cost { get; set; }
+        public string Descripion { get; set; }
+    }
+}
